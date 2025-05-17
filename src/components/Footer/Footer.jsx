f2a3bd668a4250ca555111ded0245d292fc0c90b
Marlis02 <erkinbekovmarlis70@gmail.com>
2025-05-18 01:36:47 +0600

    fix()footer max width

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,41 +6,43 @@ const Footer = () => {
   return (
     <>
       <footer className={styles.footer}>
-        <div className={styles.footer_left}>
-          <NavLink
-            to="/"
-            className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ''}`}
-          >
-            Главная
-          </NavLink>
-          <NavLink
-            to="/regions"
-            className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ''}`}
-          >
-            Регионы
-          </NavLink>
-          <NavLink
-            to="/map-chs"
-            className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ''}`}
-          >
-            Карты ЧС
-          </NavLink>
-          <NavLink
-            to="/contacts"
-            className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ''}`}
-          >
-            Контакты
-          </NavLink>
-        </div>
+        <div className={styles.con}>
+          <div className={styles.footer_left}>
+            <NavLink
+              to="/"
+              className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ''}`}
+            >
+              Главная
+            </NavLink>
+            <NavLink
+              to="/regions"
+              className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ''}`}
+            >
+              Регионы
+            </NavLink>
+            <NavLink
+              to="/map-chs"
+              className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ''}`}
+            >
+              Карты ЧС
+            </NavLink>
+            <NavLink
+              to="/contacts"
+              className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ''}`}
+            >
+              Контакты
+            </NavLink>
+          </div>
 
-        <div className={styles.sos_icons}>
-          <img src="/icons/whatsapp.svg" alt="vector" width={24} height={24} />
+          <div className={styles.sos_icons}>
+            <img src="/icons/whatsapp.svg" alt="vector" width={24} height={24} />
 
-          <img src="/icons/telegram.svg" alt="vector" width={20} height={20} />
+            <img src="/icons/telegram.svg" alt="vector" width={20} height={20} />
 
-          <img src="/icons/instagram.svg" alt="vector" width={24} height={24} />
+            <img src="/icons/instagram.svg" alt="vector" width={24} height={24} />
 
-          <img src="/icons/facebook.svg" alt="vector" width={24} height={24} />
+            <img src="/icons/facebook.svg" alt="vector" width={24} height={24} />
+          </div>
         </div>
       </footer>
       <div className={styles.footer_end}>
